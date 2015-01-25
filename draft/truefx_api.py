d4ffa7a8acb61e453f6987edb2c1a553c529d529
femtotrader <femto.trader@gmail.com>
2015-01-25 18:47:10 +0100

    Add TrueFX

--- draft/truefx_api.py
+++ draft/truefx_api.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from truefx_config import config
+"""
+API to get FX prices from TrueFX
+
+http://www.truefx.com/
+http://www.truefx.com/?page=download&description=api
+http://www.truefx.com/dev/data/TrueFX_MarketDataWebAPI_DeveloperGuide.pdf
+"""
+
+#from truefx_config import config
 #class config(object):
 #    username = "" # put your own TrueFX username
 #    password = "" # put your own TrueFX password
+
+import click
 
 import requests
 import requests_cache
@@ -18,11 +28,11 @@ def send_request(session, params):
     base_url = "http://webrates.truefx.com/rates"
     endpoint = "/connect.html"
     url = base_url + endpoint
-    #print("Request to '%s' with '%s' using '%s'" % (url, params, url+'?'+urlencode(params)))
+    print("Request to '%s' with '%s' using '%s'" % (url, params, url+'?'+urlencode(params)))
     response = session.get(url, params=params)
     return(response)
 
-def connect(session, username, password, qualifier, lst_symbols, format='csv', snapshot=True):
+def connect(session, username, password, lst_symbols, qualifier='default', format='csv', snapshot=True):
     if snapshot:
         s = 'y'
     else:
@@ -39,7 +49,7 @@ def connect(session, username, password, qualifier, lst_symbols, format='csv', s
 
     response = send_request(session, params)
     if response.status_code != 200:
-        raise(NotImplementedError)
+        raise(Exception("Can't connect"))
     
     session_data = response.text
     session_data = session_data.strip()
@@ -53,7 +63,7 @@ def disconnect(session, session_data):
     response = send_request(session, params)
     return(response)
 
-def query(session, session_data):
+def query_auth_send(session, session_data):
     params = {
         'id': session_data,
     }
@@ -62,39 +72,97 @@ def query(session, session_data):
 
 def parse_data(data):
     data_io = StringIO(data)
-    df = pd.read_csv(data_io, header=None, names=['Symbol', 'Date', 'c', 'd', 'e', 'f', 'g', 'h', 'i'])
+    df = pd.read_csv(data_io, header=None, names=['Symbol', 'Date', 'Bid', 'Bid_point', 'Ask', 'Ask_point', 'High', 'Low', 'Open'])
     df['Date'] = pd.to_datetime(df['Date'], unit='ms')
     df = df.set_index('Symbol')
     return(df)
 
-if __name__ == "__main__":
+def query_not_auth(session, lst_symbols, format='csv', snapshot=True):
+    if snapshot:
+        s = 'y'
+    else:
+        s = 'n'
+
+    params = {
+        'c': ','.join(lst_symbols),
+        'f': format,
+        's': s
+    }
+
+    response = send_request(session, params)
+    if response.status_code != 200:
+        raise(Exception("Can't connect"))
+
+    return(response)
+
+def registered(username, password):
+    return(not (username=='' and password==''))
+
+SYMBOLS_NOT_AUTH = "EUR/USD,USD/JPY,GBP/USD,EUR/GBP,USD/CHF,EUR/JPY,EUR/CHF"
+SYMBOLS_NOT_AUTH = "," + "USD/CAD,AUD/USD,GBP/JPY"
+
+SYMBOLS_ALL = "EUR/USD,USD/JPY,GBP/USD,EUR/GBP,USD/CHF,AUD/NZD,CAD/CHF,CHF/JPY,EUR/AUD"
+SYMBOLS_ALL = SYMBOLS_ALL + "," + "EUR/CAD,EUR/JPY,EUR/CHF,USD/CAD,AUD/USD,GBP/JPY,AUD/CAD"
+SYMBOLS_ALL = SYMBOLS_ALL + "," + "AUD/CHF,AUD/JPY,EUR/NOK,EUR/NZD,GBP/CAD,GBP/CHF,NZD/JPY"
+SYMBOLS_ALL = SYMBOLS_ALL + "," + "NZD/USD,USD/NOK,USD/SEK"
+
+@click.command()
+@click.option('--symbols', default='', help="Symbols list (serated with ','")
+@click.option('--username', default='', help="Username")
+@click.option('--password', default='', help="Password")
+def main(symbols, username, password):
     #session = requests.Session()
 
     expire_after = timedelta(hours=1)
     session = requests_cache.CachedSession(cache_name='cache', expire_after=expire_after)
+
+    is_registered = registered(username, password)
+    if not is_registered:
+        print("""You should register to TrueFX
+and pass username and password using CLI flag
+--username your_username
+--password your_password""")
+
+    if symbols == '':
+        if not is_registered:
+            symbols = SYMBOLS_NOT_AUTH
+        else:
+            symbols = SYMBOLS_ALL
     
-    symbols = "EUR/USD,USD/JPY"
     symbols = symbols.split(',')
-    #symbols = ["EUR/USD", "USD/JPY"]
-    #symbols = ["EUR/USD", "USD/JPY", "GBP/USD", "EUR/GBP", "USD/CHF", \
-    #"EUR/JPY", "EUR/CHF", "USD/CAD", "AUD/USD", "GBP/JPY", \
-    #"AUD/JPY", "AUD/NZD", "CAD/JPY", "CHF/JPY", "NZD/USD"]
 
     qualifier = 'default'
 
     format = 'csv'
     snapshot = True
 
-    print("Connect")
-    session_data = connect(session, config.username, config.password, qualifier, symbols, format, snapshot)
+    if not is_registered:
+        response = query_not_auth(session, symbols, format, snapshot)
+        data = response.text
+        print("Query response:\n%s" % data)
+        df = parse_data(data)
+        print("Query parsed response:\n%s" % df)
+        print(df)
+    else:
+        print("Connect")
+        #(username, password) = (config.username, config.password)
+        #(username, password) = ('', '')
+        session_data = connect(session, username, password, symbols, qualifier, format, snapshot)
+        error_msg = 'not authorized'
+        if error_msg in session_data:
+            raise(Exception(error_msg))
 
-    response = query(session, session_data)
-    data = response.text
-    print("Query response:\n%s" % data)
-    df = parse_data(data)
-    print(df)
+        response = query_auth_send(session, session_data)
+        data = response.text
+        print("Query response:\n%s" % data)
+        df = parse_data(data)
+        print("Query parsed response:\n%s" % df)
+        print(df)
 
-    print("Disconnect")
-    response = disconnect(session, session_data)
-    data = response.text
-    print(data)
+        print("Disconnect")
+        response = disconnect(session, session_data)
+        data = response.text
+        print(data)
+
+if __name__ == "__main__":
+    main()
