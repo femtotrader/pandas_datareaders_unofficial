ee17d46db19815e0e53cd3fc8978bad6eb8670ad
femtotrader <femto.trader@gmail.com>
2015-02-01 19:00:50 +0100

    Minor fix for Stooq

--- draft/stooq/main.py
+++ draft/stooq/main.py
@@ -22,6 +22,13 @@ from clint.textui import progress
 
 from six.moves import cStringIO as StringIO
 
+"""
+from main import conv_resol, conv_geo, download_data, get_data
+expire_after = timedelta(days=1)
+session = requests_cache.CachedSession(cache_name='cache', expire_after=expire_after)
+panel = get_data('poland', 'H', session)
+"""
+
 def conv_resol(resolution):
     d = {
         to_offset('5Min'): '5',
@@ -54,15 +61,13 @@ def conv_geo(geo):
         logging.warning("conv_geo returns '%s'" % geo)
         return(geo)
 
-def get_data(geo, resolution, session):
-    geo = conv_geo(geo)
-    resolution = conv_resol(resolution)
-    
+def download_data(geo, resolution, session):    
     url = "http://stooq.com/db/d/?b={resolution}_{geo}_txt".format(geo=geo, resolution=resolution)
 
     print("Request %r" % url)
 
     response = session.get(url, stream=True)
+    #path = 'temp.tmp'
     #with open(path, 'wb') as f:
     #    total_length = int(response.headers.get('content-length'))
     #    for chunk in progress.bar(response.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
@@ -70,38 +75,56 @@ def get_data(geo, resolution, session):
     #            f.write(chunk)
     #            f.flush()
 
-    #print(response.content)
+    total_length = int(response.headers.get('content-length'))
+    for chunk in progress.bar(response.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
+        pass
 
+    return(response)
+
+def get_data(geo, resolution, session):
+    geo = conv_geo(geo)
+    resolution = conv_resol(resolution)
+    response = download_data(geo, resolution, session)
     print("Request done")
 
+    print("Create stream file")
     zip_data = StringIO(response.content)
+
+    print("Creating a DataFrame per symbol")
 
     d = {}
     cols = None
     with ZipFile(zip_data, 'r') as zf:
-        filenames = zf.namelist()
-        for filename in filenames:
+        for zinfo in zf.filelist:
+            filename = zinfo.filename
             filename_short, filename_ext = os.path.splitext(filename)
             zfile = zf.open(filename)
             if filename_ext.lower() == '.txt':
-                symbol = os.path.split(filename_short)[-1].upper()
-                print("Building DataFrame for '%s' from '%s'" % (symbol, filename))
-                try:
-                    if resolution=='d':
-                        df = pd.read_csv(zfile, parse_dates=0)
-                    else:
-                        df = pd.read_csv(zfile, parse_dates=[[0, 1]])
-                        df = df.rename(columns={'Date_Time': 'Date'})
+                file_exchange = filename.split('/')[3]
+                file_symbol = os.path.split(filename_short)[-1].upper()
+                #print("Building DataFrame for '%s' at '%s' from '%s'" % (file_symbol, file_exchange, filename))
+                if  zinfo.file_size>0:
+                    try:
+                        if resolution=='d':
+                            df = pd.read_csv(zfile, parse_dates=0)
+                        else:
+                            df = pd.read_csv(zfile, parse_dates=[[0, 1]])
+                            df = df.rename(columns={'Date_Time': 'Date'})
+                        df = df.set_index('Date')
+                        df['Exchange'] = file_exchange
+                        d[file_symbol] = df
+                        if cols is None:
+                            cols = df.columns
+                    except:
+                        print("Can't build DataFrame for '%s' at '%s' from '%s'" % (file_symbol, file_exchange, filename))
+                        print(traceback.format_exc())
+                        d[file_symbol] = None
+                else:
+                    print("Can't build DataFrame for '%s' at '%s' from '%s' (empty file)" % (file_symbol, file_exchange, filename))
+                    d[file_symbol] = None
+                    
 
-                    df = df.set_index('Date')
-                    d[symbol] = df
-                    if cols is None:
-                        cols = df.columns
-
-                except:
-                    print(traceback.format_exc())
-                    d[symbol] = None
-
+    print("Create Panel from DataFrame")
     panel = pd.Panel(d)
     panel = panel.transpose(2,1,0)
 
