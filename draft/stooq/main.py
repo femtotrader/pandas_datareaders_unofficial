99acd5687d182ced889a3503643d95025104f004
femtotrader <femto.trader@gmail.com>
2015-12-22 21:02:57 +0100

    minor

--- draft/stooq/main.py
+++ draft/stooq/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+import logging
+logger = logging.getLogger(__name__)
+
 import click
 
 import os
@@ -88,12 +91,12 @@ def get_data(geo, resolution, session):
     geo = conv_geo(geo)
     resolution = conv_resol(resolution)
     response = download_data(geo, resolution, session)
-    print("Request done")
+    logger.info("Request done")
 
-    print("Create stream file")
+    logger.info("Create stream file")
     zip_data = compat.BytesIO(response.content)
 
-    print("Creating a DataFrame per symbol")
+    logger.info("Creating a DataFrame per symbol")
 
     d = {}
     cols = None
@@ -102,50 +105,53 @@ def get_data(geo, resolution, session):
         df_info = pd.DataFrame(filelist)
         df_info['filename'] = df_info[0].map(lambda x: x.filename)
         df_info['file_size'] = df_info[0].map(lambda x: x.file_size)
-        df_info['date_time'] = df_info[0].map(lambda x: x.date_time)
-        #del df_info[0]
+        df_info['date_time'] = df_info[0].map(lambda x: datetime.datetime(*x.date_time))
+        del df_info[0]
         for zinfo in filelist:
             filename = zinfo.filename
             filename_short, filename_ext = os.path.splitext(filename)
-            zfile = zf.open(filename)
-            if filename_ext.lower() == '.txt':
-                file_exchange = filename.split('/')[3]
-                file_symbol = os.path.split(filename_short)[-1].upper()
-                #print("Building DataFrame for '%s' at '%s' from '%s'" % (file_symbol, file_exchange, filename))
-                if  zinfo.file_size>0:
-                    try:
-                        if resolution=='d':
-                            df = pd.read_csv(zfile, parse_dates=0)
-                        else:
-                            df = pd.read_csv(zfile, parse_dates=[[0, 1]])
-                            df = df.rename(columns={'Date_Time': 'Date'})
-                        df = df.set_index('Date')
-                        df['Exchange'] = file_exchange
-                        d[file_symbol] = df
-                        if cols is None:
-                            cols = df.columns
-                    except:
-                        print("Can't build DataFrame for '%s' at '%s' from '%s'" % (file_symbol, file_exchange, filename.replace(' ', '\ ')))
-                        print(traceback.format_exc())
+            with zf.open(filename) as zfile:
+                if filename_ext.lower() == '.txt':
+                    file_exchange = filename.split('/')[3]
+                    file_symbol = os.path.split(filename_short)[-1].upper()
+                    #print("Building DataFrame for '%s' at '%s' from '%s'" % (file_symbol, file_exchange, filename))
+                    if  zinfo.file_size>0:
+                        try:
+                            if resolution=='d':
+                                df = pd.read_csv(zfile, parse_dates=0)
+                            else:
+                                df = pd.read_csv(zfile, parse_dates=[[0, 1]])
+                                df = df.rename(columns={'Date_Time': 'Date'})
+                            df = df.set_index('Date')
+                            df['Exchange'] = file_exchange
+                            d[file_symbol] = df
+                            if cols is None:
+                                cols = df.columns
+                        except:
+                            logger.error("Can't build DataFrame for '%s' at '%s' from '%s'" % (file_symbol, file_exchange, filename.replace(' ', '\ ')))
+                            logger.error(traceback.format_exc())
+                            d[file_symbol] = None
+                            df['Exchange'] = file_exchange
+                    else:
+                        logger.error("Can't build DataFrame for '%s' at '%s' from '%s' (empty file)" % (file_symbol, file_exchange, filename.replace(' ', '\ ')))
                         d[file_symbol] = None
                         df['Exchange'] = file_exchange
-                else:
-                    print("Can't build DataFrame for '%s' at '%s' from '%s' (empty file)" % (file_symbol, file_exchange, filename.replace(' ', '\ ')))
-                    d[file_symbol] = None
-                    df['Exchange'] = file_exchange
-    print("Create Panel from DataFrame")
+    logger.info("Create Panel from DataFrame")
     panel = pd.Panel(d)
-    panel = panel.transpose(2,1,0)
+    panel = panel.transpose(2, 1, 0)
+    panel.major_axis = panel.major_axis.map(lambda n: pd.to_datetime(str(n)))
     return(panel, df_info)
 
 @click.command()
 @click.option('--geo', default='HU', help='Geo')
 @click.option('--resolution', default='D', help='resolution')
 def main(geo, resolution):
+    #geo, resolution = 'HU', 'D'
     expire_after = timedelta(days=1)
     session = requests_cache.CachedSession(cache_name='cache', expire_after=expire_after)
-    panel = get_data(geo, resolution, session)
+    panel, df_info = get_data(geo, resolution, session)
     print(panel)
+    print(df_info)
 
 if __name__ == "__main__":
     main()
